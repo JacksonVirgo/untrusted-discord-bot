48323da662b0060fde55b2a7262ec768addb2e09
Jack Nash <jacknash42@gmail.com>
2023-07-18 13:22:32 +1000

    added whisper channel creation to test

--- src/interactions/commands/test.ts
+++ src/interactions/commands/test.ts
@@ -1,5 +1,7 @@
 import { ChannelType, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
 import { newSlashCommand } from '../../structures/BotClient';
+import { prisma } from '../../database';
+import { loadDMWebhooks } from '../events/messageCreate';
 
 const data = new SlashCommandBuilder().setName('test').setDescription('Development only. Command to test');
 data.setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
@@ -8,35 +10,12 @@ export default newSlashCommand({
 	data,
 	execute: async (i) => {
 		if (!i.guild) return i.reply({ content: 'This command can only be used in a server', ephemeral: true });
-
+		if (i.user.id != '416757703516356628') return i.reply({ content: 'This command is for development only', ephemeral: true });
 		const operatives: string[] = ['Alpha', 'Gamma', 'Zeta', 'Omikron', 'Delta', 'Echo', 'November', 'Epsilon', 'Theta', 'Sigma', 'Nightingale', 'Omega', 'Cerulea'];
 
 		await i.deferReply({ ephemeral: true });
 
-		for (const operative of operatives) {
-			if (operative === 'Alpha') continue;
-
-			const category = await i.guild.channels.create({
-				name: `Op. ${operative}`,
-				type: ChannelType.GuildCategory,
-				permissionOverwrites: [
-					{
-						id: i.guild.roles.everyone.id,
-						deny: [PermissionFlagsBits.ViewChannel],
-					},
-				],
-			});
-
-			if (!category) return i.reply({ content: 'Something went wrong', ephemeral: true });
-
-			for (const second of operatives) {
-				const channel = await i.guild.channels.create({
-					name: `op-${second.toLowerCase()}`,
-					type: ChannelType.GuildText,
-					parent: category,
-				});
-			}
-		}
+		await loadDMWebhooks(i.guild);
 
 		return i.editReply({
 			content: 'Done',
